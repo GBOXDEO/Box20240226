d9060e98fb3a1289bfea72a6d3c10d994b46a6d8
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-04-02 20:17:54 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -11,9 +11,9 @@ public class HawkConfig {
     public static final String PUSH_TO_ADDR = "push_to_addr"; // 推送到地址的IP
     public static final String PUSH_TO_PORT = "push_to_port"; // 推送到地址的端口
     // URL Configurations
-    public static final String API_URL = "api_url";
+    public static final String API_URL = "http://152.32.231.214:26999/list.txt";
     public static final String API_HISTORY = "api_history";
-    public static final String LIVE_URL = "live_url";
+    public static final String LIVE_URL = "http://152.32.231.214:26999/index.txt";
     public static final String LIVE_HISTORY = "live_history";
     public static final String EPG_URL = "epg_url";
     public static final String EPG_HISTORY = "epg_history";
